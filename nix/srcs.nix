05d65112cc6d1c837c887c0366d01c6b4f793587
captain13128 <61901168+captain13128@users.noreply.github.com>
2020-06-01 20:37:33 +0300

    Update srcs.nix

--- nix/srcs.nix
+++ nix/srcs.nix
@@ -2,7 +2,7 @@
 
 rec {
   makerpkgs = import (fetchGit {
-    url = "https://github.com/makerdao/nixpkgs-pin";
+    url = "https://github.com/makerdao/nixpkgs";
     rev = "d4b7fe56b38236566b3014d328be1bd9c7be7a2f";
     ref = "master";
   }) {
